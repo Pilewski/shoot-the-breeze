b174cdc184e8773779eb51d987bf381afc823bfb
Nick <nicknchambers@gmail.com>
2016-10-26 08:12:34 -0600

    attempts to write messagefield tests

--- test/MessageField.spec.js
+++ test/MessageField.spec.js
@@ -4,3 +4,18 @@ import { assert, expect } from 'chai';
 let sinon = require('sinon');
 const locus = require('locus');
 import MessageField from '../lib/components/MessageField';
+
+describe('MessageField', () => {
+
+  it('renders as a <div> to contain all messages', () => {
+    const wrapper = shallow(<MessageField />);
+    assert.equal(wrapper.length, 1);
+  });
+
+  it ('will filter messages based on user input', () => {
+    const wrapper = shallow(<MessageField />);
+    console.log(wrapper.debug());
+    wrapper.setState({userInput: "I love Tom Brady"});
+    expect(wrapper.state('filteredMessages').length).to.equal(1)
+  })
+});
